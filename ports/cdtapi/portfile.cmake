b2b9f25a51f6804cdf251ba906e51b991e400040
Maarten Ghijsen <maarten.ghijsen@dektec.com>
2024-11-03 00:07:52 +0100

    new version v1.1.0

--- ports/cdtapi/portfile.cmake
+++ ports/cdtapi/portfile.cmake
@@ -2,10 +2,10 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY )
 
 # CDTAPI version used by this port file.
-set(DTAPI_VERSION 6.6.0)
+set(CDTAPI_VERSION 1.1.0)
 
 # Init variabale for name of library source file to safe initial values.
-set(LIB_NAME_BASE "CDTAPI")
+set(LIB_NAME_BASE "cdtapi")
 set(LIB_NAME_ARCH "")
 set(LIB_NAME_CRT "")
 set(LIB_NAME_EXT "")
@@ -20,7 +20,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
     message(FATAL_ERROR "Features 'vc17', 'vc16' and 'vc15' are mutually exclusive. Please specify only one.")
   endif()
 
-  # Step 1: set Windows specific names and locations for DTAPI
+  # Step 1: set Windows specific names and locations for CDTAPI
   
   #x86 or x64
   if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
@@ -62,18 +62,18 @@ if(VCPKG_TARGET_IS_WINDOWS)
   set(LIB_NAME_EXT "lib")
   set(LIB_NAME_DBG_SUFFIX "d")
   
-  #Step 2: download zip with Windows binaries.
-  vcpkg_download_distfile(
-    ARCHIVE
-    URLS "https://dektec.com/products/SDK/DTAPI/Downloads/dtapi-v${DTAPI_VERSION}-windows.zip"
-    FILENAME "dtapi-v${DTAPI_VERSION}-windows.tar.gz"
-    SHA512 3EAB7649AB82C6814B3A6654B32975E13353B2A424273A508A9CEB4CDA40DD35FE9D9A0FCAB0DE4E879A5C67380D1BABD62E355B3EC6EAE9836D4FB5C2BF6C58
-  )
+  #Step 2: cdtapi from github.
+  vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO dektec-projects/cdtapi
+    REF "v${CDTAPI_VERSION}"
+    SHA512 2cadbf73c80990f36018805ad76c7b7fbdb506958d13f96f26e6fcef8717e789f2ac70be769676e3562d17115d45b32835bba84e302f26f25f872dddce200e65
+)
 
 elseif(VCPKG_TARGET_IS_LINUX)
   message(STATUS "Installing CDTAPI for Linux.")
   
-  # Step 1: set Linux specific names and locations for DTAPI.
+  # Step 1: set Linux specific names and locations for cdtapi.
   
   set(LIB_SRCDIR ".")
   
@@ -91,69 +91,28 @@ elseif(VCPKG_TARGET_IS_LINUX)
   set(LIB_NAME_DBG_SUFFIX "")
   
   #Step 2: download zip with Linux binaries.
-  vcpkg_download_distfile(
-    ARCHIVE
-    URLS "https://dektec.com/products/SDK/DTAPI/Downloads/dtapi-v${DTAPI_VERSION}-linux.tar.gz"
-    FILENAME "dtapi-v${DTAPI_VERSION}-linux.tar.gz"
-    SHA512 4CC807B8295FD92C094B78DB3F0D234CE76825097A920CF1FA6E07151BC9108E28DBD1EF1EAB3F836EBBFF1BCCBBC18611E84A2892D87EA5AEB662C0E0383BAF
-  )
   
 else()
   message(FATAL_ERROR "Only the Windows and Linux platforms are supported.")
 endif()
 
-# Step 3: extract archive with CDTAPI
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH ARCHIVE ${ARCHIVE}
-  )
-
 # Construct full library name
 set(LIB_NAME "${LIB_NAME_BASE}${LIB_NAME_ARCH}${LIB_NAME_CRT}")
 message(STATUS "Using '${LIB_SRCDIR}/${LIB_NAME}.${LIB_NAME_EXT}' as source")
 
-##############################################################################################################
-#                                                                                                            #
-# Install files                                                                                              #
-#                                                                                                            #
-##############################################################################################################
+## Configure cmake
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DVCPKG_BUILD=ON)
+## Let cmake install our targets
+vcpkg_cmake_install()
+## Moves cmake files from debug/share to /share
+vcpkg_cmake_config_fixup()
+## Deletes the duplicate include directory from the debug/include installation to prevent overlap.
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Install - CDTAPI includes
-file(GLOB INCLUDE_FILES ${SOURCE_PATH}/include/CDTAPI*.h)
-foreach(INCLUDE_FILE ${INCLUDE_FILES})
-  file(INSTALL
-   "${INCLUDE_FILE}"
-   DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT}
-  )
-endforeach()
+## install license/copyright file
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/license.md)
 
-# Install - CDTAPI debug library
-file(INSTALL
-     "${SOURCE_PATH}/lib/${LIB_SRCDIR}/${LIB_NAME}${LIB_NAME_DBG_SUFFIX}.${LIB_NAME_EXT}"
-     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-     RENAME "${LIB_NAME_BASE}.${LIB_NAME_EXT}"
-    )
-
-# Install - CDTAPI release library
-file(INSTALL
-     "${SOURCE_PATH}/lib/${LIB_SRCDIR}/${LIB_NAME}.${LIB_NAME_EXT}"
-       DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-     RENAME "${LIB_NAME_BASE}.${LIB_NAME_EXT}"
-    )
-# Install - CDTAPI copyright file
-file(INSTALL
-   "${SOURCE_PATH}/share/cdtapi/copyright"
-   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
-  )
-
-# Install - CDTAPI cmake files library
-file(GLOB SHARE_FILES ${SOURCE_PATH}/share/cdtapi/*.cmake)
-foreach(SHARE_FILE ${SHARE_FILES})
-  file(INSTALL
-  "${SHARE_FILE}"
-  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
-  )
-endforeach()
-
+############################################################
 # Check if one or more features are a part of a package installation.
 # See /docs/maintainers/vcpkg_check_features.md for more details
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
