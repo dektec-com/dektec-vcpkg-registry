e40fe42307458211144cfad56db84f78a630d2f9
Maarten Ghijsen <maarten.ghijsen@dektec.com>
2024-11-05 13:07:25 +0100

    cleanup of dtapi port-file

--- ports/dtapi/portfile.cmake
+++ ports/dtapi/portfile.cmake
@@ -1,28 +1,41 @@
-# Warn that only static linking is supported
+## *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#* portfile.cmake *#*#*#*#*#*#*#*#*#*#*#*#*#*#*# (C) 2024 DekTec
+##
+## vcpkg port file for DekTec's C++ API (aka 'DTAPI').
+
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+ Preconditions and configuration +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+
+## Warn that only static linking is supported.
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY )
 
-# DTAPI version used by this port file.
+## DTAPI version used by this port file.
 set(DTAPI_VERSION 6.6.0)
 
-# Init variabale for name of library source file to safe initial values.
+## Init variabale for name of library source file to safe initial values.
 set(LIB_NAME_BASE "DTAPI")
 set(LIB_NAME_ARCH "")
 set(LIB_NAME_CRT "")
 set(LIB_NAME_EXT "")
 set(LIB_NAME_DBG_SUFFIX "")
 
-# Determine which platform is targetted
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+ Download and extract files +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+
+## Determine which platform is targeted?
 if(VCPKG_TARGET_IS_WINDOWS)
+
   message(STATUS "Installing DTAPI for Windows.")
   
-  # Mutual exclusivity check for Windows-only features
+  ## Mutual exclusivity check for Windows-only features
   if ("vc17" IN_LIST FEATURES AND "vc16" IN_LIST FEATURES AND "vc15" IN_LIST FEATURES)
     message(FATAL_ERROR "Features 'vc17', 'vc16' and 'vc15' are mutually exclusive. Please specify only one.")
   endif()
 
-  # Step 1: set Windows specific names and locations for DTAPI
+  ## Step 1: Set Windows specific names and locations for DTAPI.
   
-  #x86 or x64
+  ## x86 or x64?
   if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
     set(LIB_NAME_ARCH "")
   elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
@@ -31,7 +44,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
     message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
   endif()
   
-  # Which visual studio verions?
+  ## Which visual studio version?
   if ("vc17" IN_LIST FEATURES)
     set(LIB_SRCDIR "vc17")
   elseif ("vc16" IN_LIST FEATURES)
@@ -39,7 +52,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
   elseif ("vc15" IN_LIST FEATURES)
     set(LIB_SRCDIR "vc15")
   else()
-    # No feature specified => auto detect based on platform toolset.
+    ## No feature specified => auto detect based on platform toolset.
     if (VCPKG_PLATFORM_TOOLSET STREQUAL "v143")
       set(LIB_SRCDIR "vc17")
     elseif (VCPKG_PLATFORM_TOOLSET STREQUAL "v142")
@@ -47,12 +60,12 @@ if(VCPKG_TARGET_IS_WINDOWS)
     elseif (VCPKG_PLATFORM_TOOLSET STREQUAL "v141")
       set(LIB_SRCDIR "vc15")
     else()
-      # Default to VC16 version.
+      ## Default to VC16 version.
       set(LIB_SRCDIR "vc16")
     endif()
   endif()
   
-  # Using dynamic or static runtimes?
+  ## Using dynamic or static runtimes?
   if (VCPKG_CRT_LINKAGE STREQUAL "dynamic")
     set(LIB_NAME_CRT "MD")
   else()
@@ -62,7 +75,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
   set(LIB_NAME_EXT "lib")
   set(LIB_NAME_DBG_SUFFIX "d")
   
-  #Step 2: download zip with Windows binaries.
+  ## Step 2: Download zip with Windows binaries.
   vcpkg_download_distfile(
     ARCHIVE
     URLS "https://dektec.com/products/SDK/DTAPI/Downloads/dtapi-v${DTAPI_VERSION}-windows.zip"
@@ -71,13 +84,14 @@ if(VCPKG_TARGET_IS_WINDOWS)
   )
 
 elseif(VCPKG_TARGET_IS_LINUX)
+
   message(STATUS "Installing DTAPI for Linux.")
   
-  # Step 1: set Linux specific names and locations for DTAPI.
+  ## Step 1: set Linux specific names and locations for DTAPI.
   
   set(LIB_SRCDIR ".")
   
-  #x86 or x64
+  ## x86 or x64?
   if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
     set(LIB_NAME_ARCH "")
   elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
@@ -90,7 +104,7 @@ elseif(VCPKG_TARGET_IS_LINUX)
   set(LIB_NAME_EXT "o")
   set(LIB_NAME_DBG_SUFFIX "")
   
-  #Step 2: download zip with Linux binaries.
+  ## Step 2: Download zip with Linux binaries.
   vcpkg_download_distfile(
     ARCHIVE
     URLS "https://dektec.com/products/SDK/DTAPI/Downloads/dtapi-v${DTAPI_VERSION}-linux.tar.gz"
@@ -102,22 +116,18 @@ else()
   message(FATAL_ERROR "Only the Windows and Linux platforms are supported.")
 endif()
 
-# Step 3: extract archive with DTAPI
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH ARCHIVE ${ARCHIVE}
-  )
+## Step 3: Extract archive with DTAPI.
+vcpkg_extract_source_archive_ex(OUT_SOURCE_PATH SOURCE_PATH ARCHIVE ${ARCHIVE})
 
-# Construct full library name
+## Construct full library name.
 set(LIB_NAME "${LIB_NAME_BASE}${LIB_NAME_ARCH}${LIB_NAME_CRT}")
 message(STATUS "Using '${LIB_SRCDIR}/${LIB_NAME}.${LIB_NAME_EXT}' as source")
 
-##############################################################################################################
-#                                                                                                            #
-# Install files                                                                                              #
-#                                                                                                            #
-##############################################################################################################
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+## =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+ Install files +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
 
-# Install - DTAPI includes
+## Install - DTAPI includes.
 file(GLOB INCLUDE_FILES ${SOURCE_PATH}/include/DTAPI*.h)
 foreach(INCLUDE_FILE ${INCLUDE_FILES})
   file(INSTALL
@@ -126,26 +136,26 @@ foreach(INCLUDE_FILE ${INCLUDE_FILES})
   )
 endforeach()
 
-# Install - DTAPI debug library
+## Install - DTAPI debug library.
 file(INSTALL
      "${SOURCE_PATH}/lib/${LIB_SRCDIR}/${LIB_NAME}${LIB_NAME_DBG_SUFFIX}.${LIB_NAME_EXT}"
      DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
      RENAME "${LIB_NAME_BASE}.${LIB_NAME_EXT}"
     )
 
-# Install - DTAPI release library
+## Install - DTAPI release library.
 file(INSTALL
      "${SOURCE_PATH}/lib/${LIB_SRCDIR}/${LIB_NAME}.${LIB_NAME_EXT}"
        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
      RENAME "${LIB_NAME_BASE}.${LIB_NAME_EXT}"
     )
-# Install - DTAPI copyright file
+## Install - DTAPI copyright file.
 file(INSTALL
    "${SOURCE_PATH}/share/copyright"
    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
   )
 
-# Install - DTAPI cmake files library
+## Install - DTAPI cmake files library.
 file(GLOB SHARE_FILES ${SOURCE_PATH}/share/*.cmake)
 foreach(SHARE_FILE ${SHARE_FILES})
   file(INSTALL
@@ -154,7 +164,7 @@ foreach(SHARE_FILE ${SHARE_FILES})
   )
 endforeach()
 
-# Rename cmake target DTAPI-static when a static CRT is used
+## Rename cmake target to DTAPI::DTAPI-static when a static CRT is used.
 if (VCPKG_CRT_LINKAGE STREQUAL "static")
   file(GLOB SHARE_FILES ${CURRENT_PACKAGES_DIR}/share/${PORT}/*.cmake)
   foreach(SHARE_FILE ${SHARE_FILES})
@@ -162,8 +172,9 @@ if (VCPKG_CRT_LINKAGE STREQUAL "static")
   endforeach()
 endif()
 
-# Check if one or more features are a part of a package installation.
-# See /docs/maintainers/vcpkg_check_features.md for more details
+## +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+## Check if one or more features are a part of a package installation.
+## See /docs/maintainers/vcpkg_check_features.md for more details
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
     tbb   WITH_TBB
